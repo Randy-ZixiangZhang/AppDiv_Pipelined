1d040671ab0dc97e5f67654298f55d0942acefc7
randy <zixiang.zhang@tum.de>
2022-06-01 10:58:28 +0200

    copy and paste to solve a weird not saving bug

---
+++ Python_src/AppDiv_Fitting.py
@@ -0,0 +1,17 @@
+import Big_LUT
+class AppDiv:
+    
+    def __init__(self,shifter:Shifter,lut:Big_LUT,arith:Arithmetic):
+        #give the bit length
+        #giving the shifter
+        #give the look up method
+        #give the arithmetic unit
+        pass
+    def execute(self):
+        #using sequential statements to execute the dataflow model
+        pass
+    @property
+    def Bit_Width(self):
+        return self.Bit_Width 
+        
+
